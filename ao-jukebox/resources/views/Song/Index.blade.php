a1098b029e989bb3bcd107d419988c5a61896641
Joel <99057548@mydavinci.nl>
2021-10-12 10:25:30 +0200

    add to playlist finished

--- ao-jukebox/resources/views/Song/Index.blade.php
+++ ao-jukebox/resources/views/Song/Index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <H1>You're songs</H1>
+                    <H1>Youre songs</H1>
 
                     <table style="width: 50%;">
                         <tr>
@@ -22,7 +22,7 @@
                     @foreach($Songs as $song)
                         <tr>
                             <td>{{$song->songname}}</td>
-                            <td>{{$song->id}}</td>
+                            <td><a href="/playlist/add/{{$song->id}}">Add to playlist</a></td>
                             <td>{{$song->created_at}}</td>
                             <td>{{$song->genres}}</td>
                             <td>{{$song->playlist}}</td>
